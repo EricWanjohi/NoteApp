6ea55267e42c79e2dac7c5fc808f20f334bb8149
Eric W <ericwanjohi.ew@gmail.com>
2020-05-10 10:53:42 +0300

    - Added onCreateOptionsMenu and onOptionsItemSelected in the NoteDetailsActivity. - Added a new edit drawable vector item in drawables folder. - Created new note_details_menu layout item with edit item. - Added a new menu_edit_text string resource item

--- app/src/main/java/ke/co/droidsense/noteapp/ui/NoteDetailsActivity.java
+++ app/src/main/java/ke/co/droidsense/noteapp/ui/NoteDetailsActivity.java
@@ -2,6 +2,8 @@ package ke.co.droidsense.noteapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,6 +35,28 @@ public class NoteDetailsActivity extends AppCompatActivity {
         //Set TextView text...
         noteTitle.setText( note_title );
         noteDesc.setText( note_desc );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate( R.menu.note_details_menu, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.edit) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected( item );
     }
 
 }
